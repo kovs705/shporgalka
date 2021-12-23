f4931f34d72b5f286d0de7d839f1acea49c376c1
kovs705 <smartkovsik@gmail.com>
2021-12-24 01:14:53 +0300

    Initial Commit

---
+++ shporgalka WatchKit Extension/shporgalkaApp.swift
@@ -0,0 +1,19 @@
+//
+//  shporgalkaApp.swift
+//  shporgalka WatchKit Extension
+//
+//  Created by Kovs on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct shporgalkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
